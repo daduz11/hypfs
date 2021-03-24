922329a54197fdfd44d7736f7566c21b76fe10d6
daduz11 <71891863+daduz11@users.noreply.github.com>
2021-03-24 18:00:06 +0100

    code cleaning and performances improvement

--- src/node.py
+++ src/node.py
@@ -8,106 +8,72 @@ class Node:
         self.id = create_binary_id(int_id)
         self.hypercube = Hypercube()
         self.objects = []
-        self._lock = threading.Lock()
+        self.lock = threading.Lock()
 
     def insert(self, keyword, obj):
         bit_keyword = create_binary_id(keyword)
         if bit_keyword == self.id:
             if obj not in self.objects:
-                with self._lock:
+                with self.lock:
                     self.objects.append(obj)
                     return 'success'
             else:
                 return 'failure'
         else:
-            best_path = self.hypercube.get_shortest_path(self.id, bit_keyword)
-            neighbor = best_path[1]
+            neighbor = self.hypercube.get_shortest_path(self.id, bit_keyword)[1]
             return request(neighbor, INSERT, {'keyword': str(keyword), 'obj': obj})
 
     def remove(self, keyword, obj):
         bit_keyword = create_binary_id(keyword)
         if bit_keyword == self.id:
             if obj in self.objects:
-                with self._lock:
+                with self.lock:
                     self.objects.remove(obj)
                     return 'success'
             else:
                 return 'failure'
         else:
-            best_path = self.hypercube.get_shortest_path(self.id, bit_keyword)
-            neighbor = best_path[1]
+            neighbor = self.hypercube.get_shortest_path(self.id, bit_keyword)[1]
             return request(neighbor, REMOVE, {'keyword': str(keyword), 'obj': obj})
 
     def pin_search(self, keyword, threshold=-1):
         bit_keyword = create_binary_id(keyword)
         if bit_keyword == self.id:
-            with self._lock:
-                if 0 < threshold < len(self.objects):
-                    return self.objects[:threshold]
-                else:
-                    return self.objects
+            return self.get_objects(threshold)
         else:
-            best_path = self.hypercube.get_shortest_path(self.id, bit_keyword)
-            neighbor = best_path[1]
+            neighbor = self.hypercube.get_shortest_path(self.id, bit_keyword)[1]
             return request(neighbor, PIN_SEARCH, {'keyword': str(keyword), 'threshold': threshold})
-    """
-    def superset_search(self, keyword, threshold, superset=None):
+
+    def superset_search(self, keyword, threshold, sender):
         bit_keyword = create_binary_id(keyword)
-        if superset is None:
-            superset = ','.join(self.hypercube.breadth_first_search(bit_keyword))
-        if self.id == bit_keyword:
-            superset = list(superset.split(','))
+        if one(bit_keyword) != one(self.id) and sender == 'user':
+            neighbor = self.hypercube.get_shortest_path(self.id, bit_keyword)[1]
+            return request(neighbor, SUPERSET_SEARCH, {'keyword': keyword, 'threshold': threshold, 'sender': 'user'})
+        else:
+            #if is_included(one(bit_keyword), one(self.id)):
             results = []
-            for target in superset:
+            objects = self.get_objects(threshold)
+            results.extend(objects)
+            threshold -= len(objects)
+            for neighbor in self.get_neighbors(bit_keyword):
                 if threshold <= 0:
                     break
-                if self.id == target:
-                    with self._lock:
-                        if 0 < threshold < len(self.objects):
-                            result = self.objects[:threshold]
-                        else:
-                            result = self.objects
-                        results.extend(result)
-                        threshold -= len(result)
                 else:
-                    best_path = self.hypercube.get_shortest_path(self.id, target)
-                    neighbor = best_path[1]
-                    result = list(request(neighbor, PIN_SEARCH, {'keyword': int(target, 2), 'threshold': threshold}).text.split(','))
-                    threshold -= len(result)
+                    result = get_response(request(neighbor, SUPERSET_SEARCH, {'keyword': keyword, 'threshold': threshold, 'sender': 'node'}).text)
                     results.extend(result)
-            return results
-        else:
-            best_path = self.hypercube.get_shortest_path(self.id, bit_keyword)
-            neighbor = best_path[1]
-            return request(neighbor, SUPERSET_SEARCH, {'keyword': keyword, 'threshold': threshold, 'superset': superset})
-    """
-
-    def superset_search(self, keyword, threshold, _from):
-        bit_keyword = create_binary_id(keyword)
-        if one(bit_keyword) != one(self.id) and _from == 'user':
-            best_path = self.hypercube.get_shortest_path(self.id, bit_keyword)
-            neighbor = best_path[1]
-            return request(neighbor, SUPERSET_SEARCH, {'keyword': keyword, 'threshold': threshold, 'from': 'user'})
-        else:
-            if b1_in_b2(bit_keyword, self.id):
-                results = []
-                if 0 < threshold < len(self.objects):
-                    result = self.objects[:threshold]
-                else:
-                    result = self.objects
-                results.extend(result)
-                threshold -= len(result)
-                print(self.get_neighbors(bit_keyword))
-                for neighbor in self.get_neighbors(bit_keyword):
-                    if threshold <= 0:
-                        break
-                    else:
-                        result = request(neighbor, SUPERSET_SEARCH, {'keyword': keyword, 'threshold': threshold, 'from': 'node'}).text.split(',')
-                        results.extend(result)
-                        threshold -= len(result)
+                    threshold -= len(result)
             return results
 
     def get_neighbors(self, keyword):
-        dfs = self.hypercube.depth_first_search(keyword)
-        idx = dfs.index(self.id)
-        return [dfs[i] for i in range(idx, len(dfs)) if hamming_distance(int(dfs[i], 2), int(self.id, 2)) == 1 and self.id < dfs[i]]
+        if SEARCH_TYPE == BFS:
+            tree = self.hypercube.breadth_first_search(keyword)
+        else:
+            tree = self.hypercube.depth_first_search(keyword)
+        return [tree[i] for i in range(tree.index(self.id), len(tree)) if hamming_distance(get_decimal(tree[i]), get_decimal(self.id)) == 1 and self.id < tree[i]]
+
+    def get_objects(self, threshold):
+        with self.lock:
+            if 0 < threshold < len(self.objects):
+                return self.objects[:threshold]
+            else:
+                return self.objects
