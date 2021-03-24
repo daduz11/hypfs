cf97ae18f26b4bb377cf26ee345e26fe505e3eeb
daduz11 <71891863+daduz11@users.noreply.github.com>
2021-03-24 16:51:11 +0100

    make_response() added to each return request

--- server.py
+++ server.py
@@ -3,6 +3,7 @@ import sys
 from flask import Flask, request
 from src.node import Node
 from src.parameters import *
+from src.utils import make_response, STRING_TYPE, LIST_TYPE
 
 app = Flask(APP_NAME)
 
@@ -11,20 +12,14 @@ app = Flask(APP_NAME)
 def request_insert():
     keyword = int(request.args.get('keyword'))
     obj = request.args.get('obj')
-    res = NODE.insert(keyword, obj)
-    if type(res) is not str:
-        res = res.text
-    return res
+    return make_response(NODE.insert(keyword, obj), STRING_TYPE)
 
 
 @app.route('/remove')
 def request_remove():
     keyword = int(request.args.get('keyword'))
     obj = request.args.get('obj')
-    res = NODE.remove(keyword, obj)
-    if type(res) is not str:
-        res = res.text
-    return res
+    return make_response(NODE.remove(keyword, obj), STRING_TYPE)
 
 
 @app.route('/pin_search')
@@ -35,24 +30,15 @@ def request_pin_search():
         res = NODE.pin_search(keyword)
     else:
         res = NODE.pin_search(keyword, int(threshold))
-    if type(res) is not list:
-        res = res.text
-    else:
-        res = ','.join(res)
-    return res
+    return make_response(res, LIST_TYPE)
 
 
 @app.route('/superset_search')
 def request_superset_search():
     keyword = int(request.args.get('keyword'))
     threshold = int(request.args.get('threshold'))
-    _from = request.args.get('from')
-    res = NODE.superset_search(keyword, threshold, _from)
-    if type(res) is not list:
-        res = res.text
-    else:
-        res = ','.join(res)
-    return res
+    sender = request.args.get('sender')
+    return make_response(NODE.superset_search(keyword, threshold, sender), LIST_TYPE)
 
 
 def parse_arguments(argv):
@@ -60,7 +46,6 @@ def parse_arguments(argv):
     parser.add_argument('port', type=int, help='Port number to which connect the node')
     return parser.parse_args(argv)
 
-import time
 
 if __name__ == '__main__':
     PORT = parse_arguments(sys.argv[1:]).port
